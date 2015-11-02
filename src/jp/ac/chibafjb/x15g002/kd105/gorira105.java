d3422576b7e188c135e4b24897dbee065b7bb2b6
unknown <x15g002@PC504N024.chiba-fjb.ac.jp>
2015-11-02 15:00:45 +0900

    課題5つ目

---
+++ src/jp/ac/chibafjb/x15g002/kd105/gorira105.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g002.kd105;
+
+public class gorira105 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+      System.out.println("氏名: 船橋　太郎（ふなばし　たろう）");
+      System.out.println("好きなスポーツ: 卓球　野球");
+      System.out.println("趣味: スポーツを見ること");
+	}
+
+}
