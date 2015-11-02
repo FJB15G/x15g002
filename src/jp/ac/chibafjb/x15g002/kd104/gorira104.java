1e3654245593fbb8d3959fb548b537e60b720069
unknown <x15g002@PC504N024.chiba-fjb.ac.jp>
2015-11-02 14:53:35 +0900

    課題4つ目

---
+++ src/jp/ac/chibafjb/x15g002/kd104/gorira104.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g002.kd104;
+
+public class gorira104 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("あいうえお　あいうえお　あいうえお");
+		System.out.println("あいうえお　あいうえお　あいうえお");
+		System.out.println("あいうえお　あいうえお　あいうえお");
+       
+	}
+
+}
