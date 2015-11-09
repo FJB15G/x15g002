1662232f4dbc3b19e7c4a55f95b2ce9949c3075d
unknown <x15g002@PC504N024.chiba-fjb.ac.jp>
2015-11-09 14:57:45 +0900

    課題17

---
+++ src/jp/ac/chibafjb/x15g002/kdT01/goriraT01.java
@@ -0,0 +1,15 @@
+package jp.ac.chibafjb.x15g002.kdT01;
+
+public class goriraT01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		
+		for(i=1;i<4;i=i+1)
+			System.out.println(i);
+		
+		
+	}
+
+}
