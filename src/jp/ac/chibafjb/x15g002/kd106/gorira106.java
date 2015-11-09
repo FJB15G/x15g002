24697526f7797257076dfa59676545a4d2d4a4b6
unknown <x15g002@PC504N024.chiba-fjb.ac.jp>
2015-11-09 14:39:07 +0900

    課題6つ目

---
+++ src/jp/ac/chibafjb/x15g002/kd106/gorira106.java
@@ -0,0 +1,17 @@
+package jp.ac.chibafjb.x15g002.kd106;
+
+public class gorira106 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("  a@@@a a@@@a");
+		System.out.println(" @@@@@@e@@@@@@");
+		System.out.println("(@@@@@@@@@@@@@)");
+		System.out.println(" @@@@@@@@@@@@@");
+		System.out.println("  @@@@@@@@@@@");
+		System.out.println("    @@@@@@@~");
+		System.out.println("     ~@@@~");
+		System.out.println("      ~@~　");
+	}
+
+}
