1a9c8cf4d7eaf832dc818d7bd7ff8a2d446dc4e2
unknown <x15g002@PC504N024.chiba-fjb.ac.jp>
2015-11-09 14:39:37 +0900

    課題７つ目

---
+++ src/jp/ac/chibafjb/x15g002/kd111/gorira111.java
@@ -0,0 +1,16 @@
+package jp.ac.chibafjb.x15g002.kd111;
+
+public class gorira111 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		
+		for(i=0;i<10;i=i+1)
+			System.out.println("石岡　大貴");
+		
+		
+		
+	}
+
+}
