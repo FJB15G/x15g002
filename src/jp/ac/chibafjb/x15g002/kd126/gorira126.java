0ab7bf154bd8bec39ef20dbd710cec83c6098f7c
unknown <x15g002@PC504N024.chiba-fjb.ac.jp>
2015-11-09 14:52:59 +0900

    課題16

---
+++ src/jp/ac/chibafjb/x15g002/kd126/gorira126.java
@@ -0,0 +1,14 @@
+package jp.ac.chibafjb.x15g002.kd126;
+
+public class gorira126 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		
+		for(i=0;i<3;i=i+1)
+			System.out.println("12345");
+		
+	}
+
+}
