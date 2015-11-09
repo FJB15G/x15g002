2d53d193986e2ec3252963116e992fb221a71bbb
unknown <x15g002@PC504N024.chiba-fjb.ac.jp>
2015-11-09 14:40:49 +0900

    課題11

---
+++ src/jp/ac/chibafjb/x15g002/kd121/gorira121.java
@@ -0,0 +1,15 @@
+package jp.ac.chibafjb.x15g002.kd121;
+
+public class gorira121 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		
+		for(i=1;i<11;i=i+1)
+			System.out.println(i);
+		
+		
+	}
+
+}
