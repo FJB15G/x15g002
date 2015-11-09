f3d11bb200077497e21b6b1926aa2114bab613a9
unknown <x15g002@PC504N024.chiba-fjb.ac.jp>
2015-11-09 14:40:16 +0900

    課題９つ目

---
+++ src/jp/ac/chibafjb/x15g002/kd113/gorira113.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g002.kd113;
+
+public class gorira113 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		
+		for(i=0;i<3;i=i+1)
+			System.out.println("*");
+	}
+
+}
